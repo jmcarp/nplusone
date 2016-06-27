a3342f8fd44024f7e3857e27b8412bd1988b1e8e
Joshua Carp <jm.carp@gmail.com>
2016-06-26 21:48:38 -0400

    Upgrade invoke to latest.

--- tasks.py
+++ tasks.py
@@ -4,7 +4,6 @@ import os
 import sys
 import webbrowser
 
-from invoke import run
 from invoke import task
 
 
@@ -13,14 +12,14 @@ build_dir = os.path.join(docs_dir, '_build')
 
 
 @task
-def test():
+def test(ctx):
     import pytest
     errcode = pytest.main(['tests'])
     sys.exit(errcode)
 
 
 @task
-def watch():
+def watch(ctx):
     """Run tests when a file changes. Requires pytest-xdist."""
     import pytest
     errcode = pytest.main(['-f'])
@@ -28,39 +27,39 @@ def watch():
 
 
 @task
-def clean():
-    run('rm -rf build')
-    run('rm -rf dist')
-    run('rm -rf nplusone.egg-info')
-    clean_docs()
+def clean(ctx):
+    ctx.run('rm -rf build')
+    ctx.run('rm -rf dist')
+    ctx.run('rm -rf nplusone.egg-info')
+    clean_docs(ctx)
     print('Cleaned up.')
 
 
 @task
-def clean_docs():
-    run('rm -rf {0}'.format(build_dir))
+def clean_docs(ctx):
+    ctx.run('rm -rf {0}'.format(build_dir))
 
 
 @task
-def browse_docs():
+def browse_docs(ctx):
     path = os.path.join(build_dir, 'index.html')
     webbrowser.open_new_tab(path)
 
 
 @task
-def docs(clean=False, browse=False, watch=False):
+def docs(ctx, clean=False, browse=False, watch=False):
     """Build the docs."""
     if clean:
-        clean_docs()
-    run('sphinx-build {0} {1}'.format(docs_dir, build_dir), pty=True)
+        clean_docs(ctx)
+    ctx.run('sphinx-build {0} {1}'.format(docs_dir, build_dir), pty=True)
     if browse:
-        browse_docs()
+        browse_docs(ctx)
     if watch:
-        watch_docs()
+        watch_docs(ctx)
 
 
 @task
-def watch_docs():
+def watch_docs(ctx):
     """Run build the docs when a file changes."""
     try:
         import sphinx_autobuild  # noqa
@@ -69,24 +68,24 @@ def watch_docs():
         print('Install it with:')
         print('    pip install sphinx-autobuild')
         sys.exit(1)
-    docs()
-    run('sphinx-autobuild {} {}'.format(docs_dir, build_dir), pty=True)
+    docs(ctx)
+    ctx.run('sphinx-autobuild {} {}'.format(docs_dir, build_dir), pty=True)
 
 
 @task
-def readme(browse=False):
-    run('rst2html.py README.rst > README.html', pty=True)
+def readme(ctx, browse=False):
+    ctx.run('rst2html.py README.rst > README.html', pty=True)
     if browse:
         webbrowser.open_new_tab('README.html')
 
 
 @task
-def publish(test=False):
+def publish(ctx, test=False):
     """Publish to the cheeseshop."""
-    clean()
+    clean(ctx)
     if test:
-        run('python setup.py register -r test sdist bdist_wheel', echo=True)
-        run('twine upload dist/* -r test', echo=True)
+        ctx.run('python setup.py register -r test sdist bdist_wheel', echo=True)
+        ctx.run('twine upload dist/* -r test', echo=True)
     else:
-        run('python setup.py register sdist bdist_wheel', echo=True)
-        run('twine upload dist/*', echo=True)
+        ctx.run('python setup.py register sdist bdist_wheel', echo=True)
+        ctx.run('twine upload dist/*', echo=True)

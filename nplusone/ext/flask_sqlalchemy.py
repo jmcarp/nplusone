f41f77aa255ba262246e57ee080c0ff62dfcd7a0
David Cramer <dcramer@gmail.com>
2017-11-21 07:16:10 -0800

    Improve Flask APIs
    This fixes a few behaviors and improves the usability of the Flask extension.
    
    - *Support factory pattern (init_app)
    - Correct teardown to handle missing listeners
    - Add ``ignore`` helper
    
    Note: this isn't fully supporting the app.extensions paradigm, but it at least will function in single-app-factory applications.

--- nplusone/ext/flask_sqlalchemy.py
+++ nplusone/ext/flask_sqlalchemy.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
-
 import six
+
 from flask import g
 from flask import request
+
+import nplusone.ext.sqlalchemy  # noqa
 
 from nplusone.core import signals
 from nplusone.core import listeners
 from nplusone.core import notifiers
-import nplusone.ext.sqlalchemy  # noqa
 
 
 def get_worker():
@@ -19,14 +20,17 @@ def get_worker():
 
 def setup_state():
     signals.get_worker = get_worker
+
+
 setup_state()
 
 
 class NPlusOne(object):
+    app = None
 
-    def __init__(self, app):
-        self.app = app
-        self.init_app()
+    def __init__(self, app=None):
+        if app:
+            self.init_app(app)
 
     def load_config(self):
         self.notifiers = notifiers.init(self.app.config)
@@ -35,8 +39,10 @@ class NPlusOne(object):
             for item in self.app.config.get('NPLUSONE_WHITELIST', [])
         ]
 
-    def init_app(self):
-        @self.app.before_request
+    def init_app(self, app):
+        self.app = app
+
+        @app.before_request
         def connect():
             self.load_config()
             g.listeners = getattr(g, 'listeners', {})
@@ -44,14 +50,18 @@ class NPlusOne(object):
                 g.listeners[name] = listener_type(self)
                 g.listeners[name].setup()
 
-        @self.app.after_request
+        @app.after_request
         def disconnect(response):
             for name, listener_type in six.iteritems(listeners.listeners):
-                listener = g.listeners.pop(name)
-                listener.teardown()
+                listener = g.listeners.pop(name, None)
+                if listener:
+                    listener.teardown()
             return response
 
     def notify(self, message):
         if not message.match(self.whitelist):
             for notifier in self.notifiers:
                 notifier.notify(message)
+
+    def ignore(self, signal):
+        return signals.ignore(getattr(signals, signal))

183f5c0ad76207d058a7e688f163b36ae23e4841
Steven Loria <sloria1@gmail.com>
2018-07-30 10:15:09 -0400

    Fix license in package metadata
    PyPI expects an OSI-specified license--not the full license text

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     package_dir={'nplusone': 'nplusone'},
     include_package_data=True,
     install_requires=REQUIRES,
-    license=read('LICENSE'),
+    license='MIT',
     zip_safe=False,
     keywords='nplusone',
     classifiers=[

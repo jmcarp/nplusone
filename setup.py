d16ac9be4dd11721e189bd2622c5bbc2ec727b5b
Michał Bielawski <michal.bielawski@apperio.com>
2020-03-03 18:20:49 +0000

    Add newer versions of Django and Python to CI.

--- setup.py
+++ setup.py
@@ -61,6 +61,8 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     test_suite='tests'
 )
